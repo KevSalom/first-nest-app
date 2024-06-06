1e799be0f2c2c3cd64ceda1cb68e5e11742f0005
PC1 <kevsalomdev@gmail.com>
2024-06-06 11:51:48 -0400

    params and error response in nest.js

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,40 +1,65 @@
-import { Controller, Delete, Get, Patch, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  RawBody,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { TasksService } from './tasks.service';
 import { request } from 'http';
+import { parse } from 'path';
 
 @Controller('tasks')
 export class TasksController {
-    tasksService: TasksService;
+  tasksService: TasksService;
 
-    constructor(tasksService:TasksService) {
-      this.tasksService = tasksService;
-    }
+  constructor(tasksService: TasksService) {
+    this.tasksService = tasksService;
+  }
 
-    @Get()
-    getTasks(@Res() response: Response, @Req() request: Request) {
-         console.log(request.url);
-         response.status(200).send(this.tasksService.getTasks());
-         return 
-    }
+  @Get()
+  getTasks(@Res() response: Response, @Query() query: any) {
+    console.log(query);
+    response.status(200).send(this.tasksService.getTasks());
+    return;
+  }
 
-    @Post()
-    createTask() {  
-        return this.tasksService.createTask();
+  @Get('/:id')
+  getTask(@Param('id') id: string) {
+    const task = this.tasksService.getTask(parseInt(id));
+    if (!task) {
+      return new NotFoundException(`Task with id ${id} not found`);
     }
+    return task;
+  }
 
-    @Delete()
-    deleteTask() {
-        return this.tasksService.deleteTask();
-    }
+  @Post()
+  createTask(@Body() body: any) {
+    console.log(body);
+    return this.tasksService.createTask(body);
+  }
 
-    @Put()
-    updateTask() {
-        return this.tasksService.updateTask();
-    }
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
 
-    @Patch()
-    updateStatusTask() {
-        return this.tasksService.updateStatusTask();
-    }
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Patch()
+  updateStatusTask() {
+    return this.tasksService.updateStatusTask();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,48 +1,45 @@
-import {Injectable} from '@nestjs/common'
+import {Injectable, NotFoundException} from '@nestjs/common'
 
 export interface Tasks {
-    id: number,
-    title: string,
-    description: string,
-    status: string,
-    createdAt: Date,
-    updatedAt: Date
+    title: string;
+    status: boolean;
+    id: number;
 }   
 
 @Injectable()
 export class TasksService {
 
+    private tasks: Tasks[] = [
+        {
+            title: 'Task 1',
+            status: true,
+            id: 1
+
+        },
+        {
+            title: 'Task 2',
+            status: false,
+            id: 2
+        },
+        {
+            title: 'Task 3',
+            status: true,
+            id: 3
+        }   
+    ]
+
     getTasks():Tasks[] {
-        return [
-            {
-                id: 1,                
-                title: 'Task 1',                
-                description: 'Task 1 description',                
-                status: 'pending',                
-                createdAt: new Date(),                
-                updatedAt: new Date()
-            },
-            {
-                id: 2,                
-                title: 'Task 2',                
-                description: 'Task 2 description',                
-                status: 'pending',                
-                createdAt: new Date(),                
-                updatedAt: new Date()
-            },            
-            {
-                id: 3,                
-                title: 'Task 3',                
-                description: 'Task 3 description',                
-                status: 'pending',                
-                createdAt: new Date(),                
-                updatedAt: new Date()
-            }
-        ]
+        return this.tasks
     }
 
-    createTask() {
-        return 'Creando una tarea'
+    getTask(id:number) {
+        const task = this.tasks.find(task => task.id === id)
+        return task
+    }
+
+    createTask(task:any) {
+        this.tasks.push({...task, id: this.tasks.length + 1})   
+        return task
     }
 
     updateTask() {  
